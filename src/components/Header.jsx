c462fc7307f080ebfa827480524256c2d5658b99
connorbarkr <connorbarkr@gmail.com>
2019-01-12 20:27:57 -0500

    First commit

---
+++ src/components/Header.jsx
@@ -0,0 +1,14 @@
+import React, { Component } from 'react';
+import '../scss/Header.scss';
+
+class Header extends Component {
+  render() {
+    return (
+      <div className="header">
+        <h1>{'mooch'}</h1>
+      </div>
+    );
+  }
+}
+
+export default Header;
